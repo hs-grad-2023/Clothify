39381bdb17fbbbd4088122200919f29713416c32
Game2njoy <sg851033@gmail.com>
2023-03-18 06:12:39 +0900

    디자인

--- muapp/forms.py
+++ muapp/forms.py
@@ -10,11 +10,11 @@ from django.contrib.auth import get_user_model
 class ProductForm(forms.ModelForm): # ModelForm 은 장고 모델 폼
     class Meta: # 장고 모델 폼은 반드시 내부에 Meta 클래스 가져야 함
         model = clothes
-        fields = ['type1', 'type2', 'tags', 'name', 'imgfile', 'details','upload_date']
+        fields = ['type1', 'type2', 'tag', 'name', 'imgfile', 'details','upload_date']
         labels = {
             'type1': '분류1',
             'type2': '분류2',
-            'tags': '태그',
+            'tag': '태그',
             'name': '이름',
             'imgfile': '사진',
             'details': '상세정보',
